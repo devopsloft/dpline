6c09e6ca0f2ba4d54ef36becad052cf45940cd32
Liora Milbaum <liora@lmb.co.il>
2019-10-01 19:56:37 +0300

    Add the date dynamically to the document added to the index

--- sub-services/engine/main.py
+++ sub-services/engine/main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import requests
-from datetime import date
+import json
+from datetime import date, datetime
 
 
 def main():
     today = date.today()
     s = requests.session()
     payload = open('/alert1.json', 'rb').read()
+    payload_json = json.loads(payload.decode('utf-8'))
+    payload_json['date'] = datetime.now().isoformat()
+    payload = json.dumps(payload_json).encode('utf-8')
     response = s.post(
         url="http://elasticsearch:9200/quality-gates-index-{0}/_doc".format(today),  # noqa
         headers={"Content-Type": "application/json"},
@@ -17,6 +21,9 @@ def main():
         print(response.content)
 
     payload = open('/alert2.json', 'rb').read()
+    payload_json = json.loads(payload.decode('utf-8'))
+    payload_json['date'] = datetime.now().isoformat()
+    payload = json.dumps(payload_json).encode('utf-8')
     response = s.post(
         url="http://elasticsearch:9200/quality-gates-index-{0}/_doc".format(today),  # noqa
         headers={"Content-Type": "application/json"},
