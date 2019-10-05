0f4891c50783ce66c20c3146f5503761f010d754
Liora Milbaum <liora@lmb.co.il>
2019-10-05 17:20:29 +0300

    Refactoring:- switch to elasticsearch python library

--- sub-services/elk/elasticsearch/deploy.py
+++ sub-services/elk/elasticsearch/deploy.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 
-import requests
-import sys
 from datetime import date
+
+from elasticsearch import Elasticsearch
 
 
 def main():
+    es = Elasticsearch("http://localhost:9200")
     today = date.today()
-    s = requests.session()
-    response = s.put(
-        url="http://localhost:9200/quality-gates-index-{0}".format(today),
-        headers={"Content-Type": "application/json"}
+    es.indices.create(
+        index="quality-gates-index-{0}".format(today),
+        ignore=400
     )
-    if response.status_code != 200:
-        print(response.content)
-        sys.exit(1)
 
 
 if __name__ == "__main__":
--- sub-services/engine/main.py
+++ sub-services/engine/main.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python3
 
-import requests
 import json
 from datetime import date, datetime
 
+from elasticsearch import Elasticsearch
+
 
 def main():
+    es = Elasticsearch("http://elasticsearch:9200")
     today = date.today()
-    s = requests.session()
     payload = open('/alert1.json', 'rb').read()
     payload_json = json.loads(payload.decode('utf-8'))
     payload_json['date'] = datetime.now().isoformat()
     payload = json.dumps(payload_json).encode('utf-8')
-    response = s.post(
-        url="http://elasticsearch:9200/quality-gates-index-{0}/_doc".format(today),  # noqa
-        headers={"Content-Type": "application/json"},
-        data=payload
+    es.index(
+        index="quality-gates-index-{0}".format(today),
+        refresh=True,
+        body=payload
     )
-    if response.status_code != 200:
-        print(response.content)
 
     payload = open('/alert2.json', 'rb').read()
     payload_json = json.loads(payload.decode('utf-8'))
     payload_json['date'] = datetime.now().isoformat()
     payload = json.dumps(payload_json).encode('utf-8')
-    response = s.post(
-        url="http://elasticsearch:9200/quality-gates-index-{0}/_doc".format(today),  # noqa
-        headers={"Content-Type": "application/json"},
-        data=payload
+    es.index(
+        index="quality-gates-index-{0}".format(today),
+        refresh=True,
+        body=payload
     )
-    if response.status_code != 200:
-        print(response.content)
 
 
 if __name__ == "__main__":
