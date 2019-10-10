e814da1b8a73f98bba404b5ad10e831080ee0be5
Liora Milbaum <liora@lmb.co.il>
2019-10-10 08:07:01 +0300

    fix rabbitmq issue - dpline user not created

--- sub-services/rabbitmq/init.sh
+++ sub-services/rabbitmq/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-( sleep 30 && \
+( sleep 60 && \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD && \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator && \
 rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ) & \
